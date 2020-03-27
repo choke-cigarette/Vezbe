dcda68dd5c56e98e471d3a41f295b62cca1aa38a
Viden Ivančev <45499963+choke-cigarette@users.noreply.github.com>
2020-03-27 20:33:56 +0100

    Zavrsavam kolac

--- kolac.cpp
+++ kolac.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#define KORAK 20
 using namespace std;
 
 enum TipPreliva{COKOLADNI = 1, COKOLADNI_SA_SLAGOM};
@@ -43,27 +43,128 @@ class Kolac{
         KolacStanje getStanje()const{
             return stanje;
         }
-        bool staviDaSePece()
-        bool ispeciKolac()
-        bool zavrsiKolac()
-        bool povecajTemperaturu()
-        bool smanjiTemperaturu()
+        TipPreliva getTipPreliva()const{
+            return preliv.getTip();
+        }
+        bool staviDaSePece(){
+            if(preliv.getTip() == COKOLADNI){
+                stanje = PECE_SE;
+                temperatura += KORAK;
+                return true;
+            }
+            return false;
+        }
+        bool ispeciKolac(){
+            if(stanje == PECE_SE){
+                stanje = U_PRIPREMI;
+                temperatura = 20;
+                return true;
+            }
+            return false;
+        }
+        bool zavrsiKolac(){
+            if(stanje == U_PRIPREMI){
+                stanje = PRIPREMLJEN;
+                return true;
+            }
+            return false;
+        }
+        bool povecajTemperaturu(){
+            if(stanje == PECE_SE){
+                temperatura += KORAK;
+                if(temperatura > 100){
+                    temperatura = 20;
+                    stanje = ZAGOREO;
+                }
+                return true;
+            }
+            return false;
+        }
+        bool smanjiTemperaturu(){
+            if(stanje == PECE_SE){
+                temperatura -= KORAK;
+                if(temperatura < 40){
+                    temperatura = 40;
+                }
+                return true;
+            }
+            return false;
+        }
         bool dodajSlag(){
             if(stanje == U_PRIPREMI){
                 return preliv.dodajSlag();
             }
-            else return false;
+            return false;
         }
         bool ukloniSlag(){
             if(stanje == U_PRIPREMI){
                 return preliv.ukloniSlag();
             }
-            else return false;
+            return false;
         }
 };
 
+void meni(Kolac &k){
+    char izbor = 0;
+    while(izbor != 'q'){
+        cout << "Funkcionalnosti:" << endl;
+        cout << "\t0) Get temperatura" << endl;
+        cout << "\t1) Get stanje" << endl;
+        cout << "\t2) Get tip preliva" << endl;
+        cout << "\t3) Stavi da se pece" << endl;
+        cout << "\t4) Skloni sa pecenja" << endl;
+        cout << "\t5) Zavrsi sa kolacem" << endl;
+        cout << "\t6) Povecaj temperaturu" << endl;
+        cout << "\t7) Smanji temperaturu" << endl;
+        cout << "\t8) Dodaj slag" << endl;
+        cout << "\t9) Ukloni slag" << endl;
+        cout << "Pritisni 'q' da bi zavrsio sa menijem" << endl << endl;
+        cout << "Vi birate: ";
+        izbor = getchar();
+        fflush(stdin);
+        cout << endl << "Ispis: ";
+        switch(izbor){
+        case '0':
+            cout << k.getTemperatura();
+            break;
+        case '1':
+            cout << k.getStanje();
+            break;
+        case '2':
+            cout << k.getTipPreliva();
+            break;
+        case '3':
+            cout << k.staviDaSePece();
+            break;
+        case '4':
+            cout << k.ispeciKolac();
+            break;
+        case '5':
+            cout << k.zavrsiKolac();
+            break;
+        case '6':
+            cout << k.povecajTemperaturu();
+            break;
+        case '7':
+            cout << k.smanjiTemperaturu();
+            break;
+        case '8':
+            cout << k.dodajSlag();
+            break;
+        case '9':
+            cout << k.ukloniSlag();
+            break;
+        case 'q':
+            return;
+        }
+        cout << endl << endl;
+    }
+
+}
+
 int main()
 {
-
+    Kolac k;
+    meni(k);
     return 0;
 }
