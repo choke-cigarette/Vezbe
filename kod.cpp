8b2d23202ecdc1b15de72a807e639485c316e2a8
Viden Ivančev <45499963+choke-cigarette@users.noreply.github.com>
2020-05-06 09:12:47 +0200

    Update kod.cpp

--- kod.cpp
+++ kod.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <string.h>
-#include <conio.h>
+#include <ctype.h>
 
 using namespace std;
 
+string capitalize(string a){
+    a[0] = toupper(a[0]);
+    return a;
+}
+
+void getset(string klasa, string tip, string prom){
+    cout << tip <<" " << "get" << capitalize(prom) << "()const;\n";
+    cout << "\t\tvoid" <<" " << "set" << capitalize(prom) << "(" << tip << ");\n";
+    cout << tip <<" "<< klasa << "::" << "get" << capitalize(prom) << "()const{\n\treturn " << prom << ";\n}\n\n";
+    cout << "void" <<" "<< klasa << "::" << "set" << capitalize(prom) << "(" << tip << " a){\n\t" << prom << " = a;\n}\n\n";
+}
+
 int main()
 {
-    vector<string> tipovi;
-    string text;
-    char slovo = '0';
-    while(slovo != '~'){
-        slovo = getch();
-        text += slovo;
+    string klasa, tip, prom;
+    {
+        cin >> klasa >> tip >> prom;
+        getset(klasa, tip, prom);
     }
-    slovo = text[0];
-    int i = 0;
-    while(slovo != '~'){
-        while(slovo == ' '){
-            slovo = text[++i];
-        }
-        while(slovo != ',' || slovo != ';'){
-            slovo = text[++i];
-        }
-    }
-    cout << text;
 }
